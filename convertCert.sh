2e6a18aef3e1805d1022e9c136a770637467df00
Isaac Pinheiro Craveiro <21311072+itsMeR4z0r@users.noreply.github.com>
2023-07-06 15:16:04 -0300

    Update convertCert.sh

--- convertCert.sh
+++ convertCert.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-
 clear
-
 # Verificar se o comando está disponível no sistema
 check_command() {
   if ! command -v "$1" >/dev/null 2>&1; then
     echo "O comando '$1' não está disponível. Instalando o pacote..."
-    if [ "$(uname)" = "Darwin" ]; then
-      brew install "$2"
-    elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-      sudo apt-get install -y "$2"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      if ! brew install "$2"; then
+        echo "Falha ao instalar o pacote '$2'."
+        exit 1
+      fi
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+      if ! sudo apt-get install -y "$2"; then
+        echo "Falha ao instalar o pacote '$2'."
+        exit 1
+      fi
     else
       echo "Sistema operacional não suportado. Instale manualmente o pacote '$2'."
       exit 1
@@ -28,7 +32,7 @@ read -p "Digite o caminho do arquivo ZIP: " zip_file
 zip_file=$(echo "$zip_file" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e "s/'//g")
 
 # Verificar se o arquivo ZIP existe
-if [ ! -f "$zip_file" ]; then
+if [[ ! -f "$zip_file" ]]; then
   echo "Arquivo ZIP não encontrado. Certifique-se de fornecer um caminho válido."
   exit 1
 fi
@@ -47,7 +51,8 @@ temp_folder=$(mktemp -d)
 unzip "$zip_file" -d "$temp_folder"
 
 # Verificar se os arquivos foram descompactados corretamente
-if [ ! -f "$temp_folder/private.key" ] || [ ! -f "$temp_folder/certificate.crt" ] || [ ! -f "$temp_folder/ca_bundle.crt" ]; then
+if [[ ! -f "$temp_folder/private.key" || ! -f "$temp_folder/certificate.crt" || ! -f "$temp_folder/ca_bundle.crt" ]]; then
+```bash
   echo "Erro ao descompactar os arquivos. Verifique o conteúdo do arquivo ZIP."
   rm -rf "$temp_folder"
   exit 1
@@ -70,7 +75,7 @@ pfx_file="${zip_dir}/${pfx_filename}"
 openssl pkcs12 -export -in "$temp_folder/certificate.crt" -inkey "$temp_folder/private.key" -certfile "$temp_folder/ca_bundle.crt" -out "$pfx_file" -password pass:"$cert_password"
 
 # Verificar se o arquivo PFX foi gerado corretamente
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
   echo "Erro ao converter os arquivos para formato PFX."
   rm -rf "$temp_folder"
   exit 1
